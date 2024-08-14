4aa6cbc5c0ce7733f7b03b848f9f90b04883c882
adelaida0902 <adelaida.isaeva.ch@gmail.com>
2024-08-14 14:04:58 -0500

    photos added

--- Tasks and HW/index.js
+++ Tasks and HW/index.js
@@ -16,11 +16,54 @@
     
 // }; myInput.value = '';
 
-const tasks = [
-    {id: 1,
-    title: "Task 1",
-    status: "In progress"
-    }
-];
 
-const ul = document.querySelector('ul');
+const tasks = [];
+
+const ul = document.querySelector("ul");
+const input = document.querySelector("input");
+const addBtn = document.querySelector("button");
+const h1 = document.querySelector("h1");
+
+function checkTasks() {
+  if (tasks.length === 0) {
+    h1.style.visibility = "visible";
+  } else {
+    h1.style.visibility = "hidden";
+  }
+}
+
+function renderTasks() {
+  checkTasks();
+  ul.innerHTML = "";
+
+  tasks.forEach(({ id, status, title }) => {
+    const li = `<li>
+  <span># ${id}</span> 
+  <span>${title}</span> 
+   <span>${status}</span> 
+  <button id=${id}>Delete</button>
+  </li>`;
+
+    ul.insertAdjacentHTML("beforeend", li);
+  });
+
+  document.querySelectorAll("li button").forEach((btn, index) => {
+    btn.addEventListener("click", (e) => {
+      tasks.splice(index, 1);
+      renderTasks();
+    });
+  });
+}
+
+renderTasks();
+
+addBtn.addEventListener("click", () => {
+  const newTask = {
+    id: tasks.length + 1,
+    title: input.value,
+    status: "In Progress",
+  };
+  tasks.push(newTask);
+  renderTasks();
+  input.value = "";
+});
