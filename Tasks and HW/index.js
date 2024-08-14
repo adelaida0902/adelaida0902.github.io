85f4101db37e9c19e2750d9a0a300c0cf0ec268d
adelaida0902 <adelaida.isaeva.ch@gmail.com>
2024-08-14 02:33:54 -0500

    updated

--- Tasks and HW/index.js
+++ Tasks and HW/index.js
@@ -1,21 +1,26 @@
-const myInput = document.querySelector('input');
-const listContainer = document.querySelector('ul')
-function addTask() {
-    if (myInput.value === "") {
-      alert("You must write task!");
-    } else {
-      let li = document.createElement("li");
-      li.innerHTML = myInput.value;
-      listContainer.append(li);
-      let deleteButton = document.createElement("button");
-      deleteButton.textContent = "Delete";
-      listContainer.append(li);
-    } myInput.value = " ";
-}; 
+// const myInput = document.querySelector('input');
+// const listContainer = document.querySelector('ul')
+// function addTask() {
+//     if (myInput.value === "") {
+//       alert("You must write task!");
+//     } else {
+//       let li = document.createElement("li");
+//       li.innerHTML = myInput.value;
+//       listContainer.append(li);
+//       let deleteButton = document.createElement("button");
+//       deleteButton.textContent = "Delete";
+//       listContainer.append(li, deleteButton);
+      
 
-const deleteButton = document.createElement('button');
-deleteButton.textContent = 'Delete';
-deleteButton.className = 'delete-btn';
-deleteButton.addEventListener('click', function() {
-    taskList.removeChild(listItem);
-});
+//     } myInput.value = " ";
+    
+// }; myInput.value = '';
+
+const tasks = [
+    {id: 1,
+    title: "Task 1",
+    status: "In progress"
+    }
+];
+
+const ul = document.querySelector('ul');
